4e608a6cf4957a78420050ab8eb2e66d4a3c7488
Shobster <danilosimeunovic2@gmail.com>
2019-07-27 12:55:57 +0200

    Update_6

--- Forma/Controllers/HomeController.cs
+++ Forma/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace Forma.Controllers
             return View();
         }
 
+        public IActionResult Proizvod()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
